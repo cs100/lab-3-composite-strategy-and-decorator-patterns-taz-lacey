db94d6f0ef3fbab41696c9dee2b7985ed3314d77
vjain003 <vjain003@ucr.edu>
2018-02-05 20:54:49 +0000

    Removed unnecessary .h files, formed all into component.h

--- component.h
+++ component.h
@@ -10,4 +10,102 @@ class Base {
         virtual double evaluate() = 0;
 };
 
+class op : public Base { 
+    public:
+        double value;
+
+        /* Constructors */
+        op(double value) {
+            this->value = value;
+        }
+
+        double evaluate() {
+            return value;
+        }
+};
+
+
+class multiply : public Base {
+    protected:
+    	Base* lhs;
+    	Base* rhs;
+
+    public:
+        /* Constructors */
+        multiply(Base* lhs, Base* rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+
+        double evaluate() {
+            return lhs->evaluate() * rhs->evaluate();
+        }
+};
+
+class divide : public Base {
+    protected:
+        Base* lhs;
+        Base* rhs;
+
+    public:
+        /* Constructors */
+        divide(Base* lhs, Base* rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+
+        double evaluate() {
+            return lhs->evaluate() / rhs->evaluate();
+        }
+};
+
+class add : public Base {
+    protected:
+        Base* lhs;
+        Base* rhs;
+
+    public:
+        /* Constructors */
+        add(Base* lhs, Base* rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+
+        double evaluate() {
+            return lhs->evaluate() + rhs->evaluate();            
+        }
+};
+
+class subtract : public Base {
+    protected:
+        Base* lhs;
+        Base* rhs;
+
+    public:
+        /* Constructors */
+        subtract(Base* lhs, Base* rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+
+        double evaluate() {
+            return lhs->evaluate() - rhs->evaluate();
+        }
+};
+
+class square : public Base {
+    protected:
+        Base* expression;
+
+    public:
+        /* Constructors */
+        square(Base* expression) {
+            this->expression = expression;
+        }
+
+        double evaluate() {
+            return expression->evaluate() * expression->evaluate();
+        }
+};
+
 #endif // __COMPONENT_H__
