9adbd3adb703c6b595dd585a63b1ec4ce1a038ab
Lacey Kisselburgh <lkiss001@ucr.edu>
2018-02-06 02:29:49 +0000

    Implemented add, print, swap, at, and size in vector_container.cpp

--- vector_container.cpp
+++ vector_container.cpp
@@ -5,10 +5,18 @@
 using namespace std;
 
 void vector_container::add_element(Base* element) {
-    
+    vector_values.push_back(element);
 }
 
 void vector_container::print() {
+    if (vector_values.size() == 0) {
+        cout << "ERROR, SIZE IS ZERO" << endl;
+        return;
+    }
+    
+    for (int x = 0; x < vector_values.size(); ++x) {
+        cout << vector_values.at(x)->evaluate() << ", ";
+    }
     
 }
 
@@ -17,13 +25,23 @@ void vector_container::sort() {
 }
 
 void vector_container::swap(int i, int j) {
+    cout << "Before Swap: " << endl;
+    cout << "i is:" << vector_values.at(i) << endl;
+    cout << "j is:" << vector_values.at(j) << endl;
     
+    Base* temp = vector_values.at(i);
+    vector_values.at(i) = vector_values.at(j);
+    vector_values.at(j) = temp;
+    
+    cout << "Post swap:" << endl;
+    cout << "i is:" << vector_values.at(i) << endl;
+    cout << "j is:" << vector_values.at(j) << endl;
 }
 
 Base* vector_container::at(int i) {
-    
+    return vector_values.at(i);
 }
 
 int vector_container::size() {
-    
+    return vector_values.size();
 }
