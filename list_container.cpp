83e408ed772c06a2d23e9780a8f5b6fe58b52900
Lacey Kisselburgh <lkiss001@ucr.edu>
2018-02-07 06:09:16 +0000

    implemented add, print, swap, at, and size in list_container.cpp

--- list_container.cpp
+++ list_container.cpp
@@ -9,7 +9,9 @@ void list_container::add_element(Base* element) {
 }
 
 void list_container::print() {
-    
+    for (list<Base*>::iterator it = list_values.begin(); it != list_values.end(); ++it) {
+        cout << (*it)->evaluate() << ", ";
+    }
 }
 
 void list_container::sort() {
@@ -17,13 +19,27 @@ void list_container::sort() {
 }
 
 void list_container::swap(int i, int j) {
-    
+    list<Base*>::iterator it1 = list_values.begin();
+    list<Base*>::iterator it2 = list_values.begin();
+    for (int x = 0; x < i; ++x) {
+        ++it1;
+    }
+    for (int y = 0; y < j; ++y) {
+        ++it2;
+    }
+    Base* temp = *it1;
+    *it2 = *it1;
+    *it1 = temp;
 }
 
 Base* list_container::at(int i) {
-    
+    list<Base*>::iterator it = list_values.begin();
+    for (int x = 0; x < i; ++x) {
+        ++it;
+    }
+    return *it;
 }
 
 int list_container::size() {
-    
+    return list_values.size();
 }
