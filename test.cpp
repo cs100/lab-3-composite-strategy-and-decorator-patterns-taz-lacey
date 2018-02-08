1ecf9ccfba00bf57fbca8234dcde2e9e0c54ce83
vjain003 <vjain003@ucr.edu>
2018-02-08 21:14:26 +0000

    updated with test statements for add, subtract, multiply, divide, ceil, floor, and abs

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include "vector_container.h"
 #include "list_container.h"
 #include "strategy.h"
+#include "decorator.h"
 
 using namespace std;
 
@@ -11,10 +12,29 @@ int main() {
     op* op2 = new op(2);
     op* op3 = new op(4);
     op* op4 = new op(6);
+    op* op5 = new op(5.3);
+    op* op6 = new op(6.8);
     multiply* A = new multiply(op1, op2);
     subtract* B = new subtract(op3, op4);
     add* C = new add(A, B);
     square* D = new square(C);
+    
+    Ceil* E = new Ceil(op5);
+    Floor* F = new Floor(op6);
+    Abs* G = new Abs(B);
+  
+   //** test statements
+   cout << endl << "Testing" << endl;
+   cout << "A is: " << A -> evaluate() << endl;
+   cout << "B is: " << B -> evaluate() << endl;
+   cout << "C is: " << C -> evaluate() << endl;
+   cout << "D is: " << D -> evaluate() << endl;
+   cout << "E is: " << E -> evaluate() << endl;
+   cout << "F is: " << F -> evaluate() << endl;
+   cout << "G is: " << G -> evaluate() << endl;
+   cout << "End Testing" << endl << endl;
+   
+
 
     vector_container* v_container = new vector_container();
     list_container* l_container = new list_container();
